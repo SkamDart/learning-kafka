41e0dc15faa520d7cfde3e0b388a8cfbb350ea3b
Cameron Dart <camerond@fair.com>
2018-09-23 14:19:20 -0700

    hello world kafka producer with docker-compose

---
+++ intro/src/main/java/com/github/skamdart/kafka/ProducerDemo.java
@@ -0,0 +1,36 @@
+package com.github.skamdart.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        String bootstrapServer = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        // create producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // send data
+        producer.send(record);
+
+        // flush
+        producer.flush();
+
+        // flush and close
+        producer.close();
+    }
+}
